23221044933e174eaf82bb47780739e55851dec9
AHNO <albnaahmdsam0@gmail.com>
2025-04-07 12:09:58 +0200

    encrypt_Message

--- server.js
+++ server.js
@@ -8,12 +8,24 @@ import fs from 'fs';
 import {body,validationResult} from "express-validator"
 import path from "path";
 import { fileURLToPath } from "url";
+import crypto from "crypto";
+dotenv.config();
+// Function to encrypt a message
+function encryptMessage(message, key) {
+  const algorithm = 'aes-256-cbc';
+  const iv = Buffer.from(process.env.IV,"hex");
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, 'hex'), iv);
 
+  let encrypted = cipher.update(message, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  return encrypted;
+}
 // Recreate __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -31,7 +43,8 @@ app.prepare().then(() => {
         if (!er.isEmpty()) {
             return res.status(400).json({status:"FAIL",data:er.array()});
         }
-        save_message(req.body.msg);
+        const encryptedMessage = encryptMessage(req.body.msg,process.env.KEY_ENC);
+        save_message(encryptedMessage);
         res.json({ message: 'Hi!' });
     });
     server.get('/images/my_photo.jpg',(req,res)=>{
